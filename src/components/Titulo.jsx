d3ff923f9c36d0db2182b1a04d69cf63cbc8aec2
Eric_Cirimarco <eric.cirimarco@gmail.com>
2024-12-12 14:58:48 -0300

    Update Titulo.jsx

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -3,7 +3,7 @@ const Titulo = () => {
     <div className="row justify-content-md-center">
       <div className="col-md-12">
         <h1 className="mt-3 mb-5 text-center">
-          Mi TO DO List con ReactJS 🚀 <hr />
+          listado de cursos Inscripciones 🚀 <hr />
         </h1>
       </div>
     </div>
